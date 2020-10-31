a77157e63187ecffd405933cd282e7448ffb1569
Fadia Rani <fadiarani9@gmail.com>
2020-10-31 14:43:35 +0700

    Add project files.

---
+++ Server_Fadia Rani_20180140069/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P2_20180140069;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace Server_Fadia_Rani_20180140069
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
